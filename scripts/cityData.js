cf24465cf04c1cef5f30743d4b4f21e8304e59b9
AndersonKailani <anderson.kailani99@gmail.com>
2024-06-05 07:35:11 -0400

    added more cities

--- scripts/cityData.js
+++ scripts/cityData.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'Atlanta', lat: 33.7488, lon: -84.3877},
         { name: 'San Diego', lat:32.7157, lon: -117.1611},
         { name: 'Accident', lat:39.6287, lon: -79.3198},
+        { name: 'Rough and Ready', lat:39.2302, lon: -121.1361},
+        { name: 'Hell', lat:42.4338, lon: -83.9845},
         // Add more cities as needed
     ];
 
